d8f26049652687765ec1eccd7e03d78eadaf05cc
b2ogeyman <kostyamyagkov@gmail.com>
2016-04-30 14:51:02 -0700

    changed dimensions

--- training_input.py
+++ training_input.py
@@ -11,19 +11,20 @@ def getdata(tar, filename):
     white = np.zeros((19, 19), dtype=np.int)
     ko = np.zeros((19, 19), dtype=np.int)
     answer = np.zeros((19, 19), dtype=np.int)
-    res = []
+    positions, moves = [], []
     starts = [m.start() for m in re.finditer(';', data)]
     for i in starts[1:]:
         x, y = ord(data[i + 3]) - ord('a'), ord(data[i + 4]) - ord('a')
         answer[x][y] = 1
         if data[i + 1] == 'B':
-            res.append([np.copy(black), np.copy(white), ko, np.copy(answer)])
+            positions.append([np.copy(black), np.copy(white), ko])
             black[x][y] = 1
         else:
-            res.append([np.copy(white), np.copy(black), ko, np.copy(answer)])
+            positions.append([np.copy(white), np.copy(black), ko])
             white[x][y] = 1
+        moves.append(np.copy(answer))
         answer[x][y] = 0
-    return res
+    return [positions, moves]
 
 # res = getdata(tar, "00010.sgf")
 # arr = res[11][3]
