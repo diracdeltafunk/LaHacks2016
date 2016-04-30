ee6b5fc7e2f1ba93b23596ef0787c58e94b54e8b
b2ogeyman <kostyamyagkov@gmail.com>
2016-04-30 15:44:19 -0700

    Reshaped dimensions

--- training_input.py
+++ training_input.py
@@ -10,6 +10,7 @@ def getdata(tar, filename):
     black = np.zeros((19, 19), dtype=np.int)
     white = np.zeros((19, 19), dtype=np.int)
     ko = np.zeros((19, 19), dtype=np.int)
+    pos = np.zeros((19, 19, 3), dtype=np.int)
     answer = np.zeros((19, 19), dtype=np.int)
     positions, moves = [], []
     starts = [m.start() for m in re.finditer(';', data)]
@@ -17,10 +18,20 @@ def getdata(tar, filename):
         x, y = ord(data[i + 3]) - ord('a'), ord(data[i + 4]) - ord('a')
         answer[x][y] = 1
         if data[i + 1] == 'B':
-            positions.append([np.copy(black), np.copy(white), ko])
+            for i in range(19):
+                for j in range(19):
+                    pos[i][j][0] = black[i][j]
+                    pos[i][j][1] = white[i][j]
+                    pos[i][j][2] = ko[i][j]
+            positions.append(np.copy(pos))
             black[x][y] = 1
         else:
-            positions.append([np.copy(white), np.copy(black), ko])
+            for i in range(19):
+                for j in range(19):
+                    pos[i][j][0] = white[i][j]
+                    pos[i][j][1] = black[i][j]
+                    pos[i][j][2] = ko[i][j]
+            positions.append(np.copy(pos))
             white[x][y] = 1
         moves.append(np.copy(answer))
         answer[x][y] = 0
