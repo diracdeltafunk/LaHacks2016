94336705b7e659b47b8a2eb7f6b4bbd1638e7905
b2ogeyman <kostyamyagkov@gmail.com>
2016-04-30 17:03:29 -0700

    All is numpy now

--- training_input.py
+++ training_input.py
@@ -2,6 +2,7 @@ import numpy as np
 import tarfile
 import re
 
+
 def getdata(tar, filename):
     member = tar.getmember("pro/" + filename)
     f = tar.extractfile(member)
@@ -11,8 +12,10 @@ def getdata(tar, filename):
     ko = np.zeros((19, 19), dtype=np.int)
     pos = np.zeros((19, 19, 3), dtype=np.int)
     answer = np.zeros((19, 19), dtype=np.int)
-    positions, moves = [], []
     starts = [m.start() for m in re.finditer(';', data)]
+    length = len(starts) - 1
+    j = 0
+    positions, moves = np.zeros((length, 19, 19, 3)), np.zeros((length, 19, 19))
     for i in starts[1:]:
         x, y = ord(data[i + 3]) - ord('a'), ord(data[i + 4]) - ord('a')
         answer[x][y] = 1
@@ -22,7 +25,7 @@ def getdata(tar, filename):
                     pos[i][j][0] = black[i][j]
                     pos[i][j][1] = white[i][j]
                     pos[i][j][2] = ko[i][j]
-            positions.append(np.copy(pos))
+            positions[j] = np.copy(pos)
             black[x][y] = 1
         else:
             for i in range(19):
@@ -30,10 +33,11 @@ def getdata(tar, filename):
                     pos[i][j][0] = white[i][j]
                     pos[i][j][1] = black[i][j]
                     pos[i][j][2] = ko[i][j]
-            positions.append(np.copy(pos))
+            positions[j] = np.copy(pos)
             white[x][y] = 1
-        moves.append(np.copy(answer))
+        moves[j] = np.copy(answer)
         answer[x][y] = 0
+        j += 1
     return [positions, moves]
 
 # res = getdata(tar, "00010.sgf")
